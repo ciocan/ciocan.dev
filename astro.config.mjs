19c778ae6c8855ef59ab5af5488cd090cf1a77fd
Radu Ciocan <ciocan@gmail.com>
2025-01-27 14:12:44 +0000

    add layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,37 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
+
+import { SITE_URL } from "./src/consts";
 
 export default defineConfig({
+  site: SITE_URL,
+
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@/": "./src/*",
+      },
+    },
   },
 
-  integrations: [react()],
+  integrations: [react(), mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha",
+      },
+    },
+  },
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_URL = "https://ciocan.dev";
+export const SITE_TITLE = "ciocan.dev";
+export const SITE_DESCRIPTION = "ciocan.dev";
+export const EMAIL = "hello@ciocan.dev";
