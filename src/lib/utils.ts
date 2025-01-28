78ad8e42f543eb73774c1bd26ebc1b8905b48b89
Radu Ciocan <ciocan@gmail.com>
2025-01-28 20:44:21 +0000

    refactor layout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,11 +9,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date) {
-  return Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
+  return date.toLocaleDateString(undefined, {
+    year: "2-digit",
+    month: "short",
     day: "numeric",
-  }).format(date);
+  });
 }
 
 export function externalAnchorPlugin() {
